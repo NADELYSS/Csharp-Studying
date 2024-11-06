c5291572fb3c59425c68ef4b851b31aa3febd410
NADELYSS <yj.cho05120553@gmail.com>
2024-11-06 16:26:36 +0900

    Day 1

---
+++ Study_Day_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("I'm Learning C#");
+            Console.WriteLine("It's awesome!\n");
+
+            Console.WriteLine(3 + 3);
+
+            Console.Write("Hello World!  ");
+            Console.Write("I will print on the same line.");
+
+        }
+
+    }
+
+}
